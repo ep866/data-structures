317038a5738db76827c9a8f0e4085b6cfa7298e3
Elena <e_paunova@yahoo.com>
2016-09-22 01:18:22 -0400

    formatted the meetings and exported in json file

--- assignment_4/dbInsert.js
+++ assignment_4/dbInsert.js
@@ -1,7 +1,8 @@
 var fs = require("fs");
 var request = require("request");
 var mongodb = require("mongodb");
-
+var MongoClient = mongodb.MongoClient;
+var _ = require("underscore");
 
 //*******************************************
 // Utils
@@ -16,6 +17,27 @@ var data = {
 
     format: function(data) {
 
+        var allGroups = _.map(data, function(group){
+
+            return _.map(group.meetings, function(thisGroup){
+
+                var g = thisGroup;
+
+                g.day = g.time.split(",")[0];
+                g.time = g.time.split(",")[1].trim();
+                g.latLong = group.latLong;
+                g.formattedLocation = group.formattedLocation;
+
+                return g;
+            });
+        });
+
+        var collection = _.flatten(allGroups);
+
+        console.log(collection);
+        // write file
+        fs.writeFileSync('./db_collection.json', JSON.stringify(collection, null, 2) , 'utf-8');
+
     }
 };
 
@@ -23,6 +45,8 @@ var db = {
 
 };
 
+// to visualize the data I will need all meetings for the applicable day and time
+// and then have to group again by location
 
 data.readFile("augmented_meetings.json", function(meetings){
 
