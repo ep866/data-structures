85256ff2a2f602c2e76082c11e61dbfc947400bc
Elena <e_paunova@yahoo.com>
2016-09-28 20:15:53 -0400

    add assignment_5 progress

---
+++ assignment_5/mongoDb.js
@@ -0,0 +1,171 @@
+var fs = require("fs");
+var request = require("request");
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var _ = require("underscore");
+
+//*******************************************
+// Data manipulation
+//*******************************************
+
+var data = {
+    readFile: function(file, cb) {
+        var content = fs.readFileSync(file);
+
+        return cb(JSON.parse(content));
+    },
+
+    format: function(data) {
+
+        var allGroups = _.map(data, function(group){
+
+            return _.map(group.meetings, function(thisGroup){
+
+                var g = thisGroup;
+                var time = g.time.split(",")[1].trim();
+                var startTime = time.split(" to ")[0].trim();
+                var endTime = time.split(" to ")[1].trim();
+
+                // save detailed location
+                g.fullLocation = g.location;
+                // save just the name of the meeting place
+                g.location = group.location
+                // get day of meeting
+                g.day = g.time.split(",")[0];
+                // get start time
+                g.startTime = startTime;
+                // get end time
+                g.endTime = endTime;
+                // get lat long
+                g.latLong = group.latLong;
+                // get formatted location
+                g.formattedLocation = group.formattedLocation;
+                // discard the time key
+                delete g.time;
+
+                return g;
+            });
+        });
+
+        var collection = _.flatten(allGroups);
+
+        // write file
+        fs.writeFileSync('./db_collection.json', JSON.stringify(collection, null, 2) , 'utf-8');
+
+
+        console.log("******************************************");
+        console.log("Wrote db_collection.json with formated data");
+        console.log("******************************************");
+
+    }
+};
+
+//*******************************************
+// DB stuff
+//*******************************************
+var collectionName = "meetings";
+var dbName = "AAmeetings";
+var url = "mongodb://localhost:27017/" + dbName;
+
+var db = {
+    insert: function(data) {
+        // console.log(data.length);
+
+        // connect
+        MongoClient.connect(url, function(err, db){
+            if(err) { return console.log(err); }
+
+            var collection = db.collection(collectionName);
+
+            // insert
+            data.forEach(function(meeting, i){
+                collection.insert( meeting );
+            });
+
+            db.close();
+        });
+    },
+    aggregate: function(testId, nIterations) {
+        var datetimeStart = new Date();
+
+        MongoClient.connect(url, function(err, db) {
+            if (err) {return console.dir(err);}
+
+            var collection = db.collection(collectionName);
+
+            collection.aggregate([{ $limit : 3 }]).toArray(function(err, docs) {
+                if(err) {
+                    console.log(err)
+                } else {
+                   console.log(docs);
+                }
+
+                profiler.result.push({
+                    id: testId,
+                    time: new Date() - datetimeStart
+                });
+                if(testId == nIterations) {
+                    profiler.output(nIterations);
+                }
+
+            });
+
+            db.close();
+        });
+
+    }
+};
+
+var profiler = {
+    result: [],
+
+    test:function(nIterations) {
+
+        for(var i = 0; i <= nIterations; i++) {
+            db.aggregate(i,nIterations);
+        }
+
+    },
+    output: function(nIterations) {
+        console.log("******************************************");
+        console.log(profiler.result);
+        console.log("******************************************");
+        // write file
+        fs.writeFileSync('./profiler/test_' + nIterations + '_times.json', JSON.stringify(profiler.result, null, 2) , 'utf-8');
+    }
+
+};
+
+
+data.readFile("augmented_meetings.json", function(meetings){
+
+    //*******************************************
+    // Format meetings for collection insertion
+    //*******************************************
+
+    // data.format(meetings);
+
+});
+
+
+data.readFile("db_collection.json", function(meetings){
+
+    //*******************************************
+    // Now insert the formatted meetings into the DB
+    //*******************************************
+
+    // db.insert(meetings);
+
+});
+
+
+profiler.test(10);
+
+
+
+
+
+
+
+
+
